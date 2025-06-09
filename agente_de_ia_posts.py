5496d07b2eb5495b898d425c56a7b158a4f12b9a
Michelle Duso <chelleduso@gmail.com>
2025-06-09 20:42:34 -0300

    Configuring OPEN IA KEY

--- agente_de_ia_posts.py
+++ agente_de_ia_posts.py
@@ -2,3 +2,13 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 import os
+
+OPENAI_KEY = os.getenv("API_KEY")
+
+chat = ChatOpenAI(
+    model_name = "gpt-4o-mini",
+    openai_api_key = OPENAI_KEY,
+    temperature=0.7
+
+)
+
